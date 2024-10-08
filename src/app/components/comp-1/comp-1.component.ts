8794403babe93f2cbe8c1ca4f0b9112be33628ef
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-08 16:55:36 +0200

    first commit

---
+++ src/app/components/comp-1/comp-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-1',
+  templateUrl: './comp-1.component.html',
+  styleUrl: './comp-1.component.scss'
+})
+export class Comp1Component {
+
+}
---
+++ src/app/components/comp-2/comp-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-2',
+  templateUrl: './comp-2.component.html',
+  styleUrl: './comp-2.component.scss'
+})
+export class Comp2Component {
+
+}
